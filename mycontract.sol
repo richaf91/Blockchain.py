d83219a3e656017e1fc851cc6e08711521709bd2
Richard Brito <72542836+richaf91@users.noreply.github.com>
2022-04-14 10:22:25 -0400

    Create mycontract.sol

---
+++ mycontract.sol
@@ -0,0 +1,12 @@
+Practice with solidut
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.0;
+
+contract Property {
+    int public value;
+
+    function setValue(int _value) public{
+        value = _value;
+    }
+}
